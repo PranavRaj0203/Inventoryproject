8df3fb21ecd52999e0175eb24a97b855cea25d40
Pranav Raj <pranavraj0203@gmail.com>
2024-02-12 13:12:48 +0530

    Middleware and routes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,14 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express()
-
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json());
+// Routes
+app.get("/",(req,res)=>{
+    res.send("Home Page");
+});
 const PORT = process.env.PORT || 5000;
 
 // connect to db and server start
