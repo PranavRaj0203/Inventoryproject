f37ac13945e1a4d618be16116272601576ef5ecd
Pranav Raj <pranavraj0203@gmail.com>
2024-04-26 14:04:43 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute")
 const productRoute = require("./routes/productRoute");
-const contactRoute = require("./routes/contactRoute");
 const errorHandler=require("./middleWare/errorMiddleware")
 const cookieParser=require("cookie-parser");
 const path = require("path");
@@ -20,7 +19,7 @@ app.use(bodyParser.json());
 //route middleware
 app.use("/api/users",userRoute);
 app.use("/api/products",productRoute);
-app.use("/api/contactus",contactRoute);
+
 // Routes
 app.get("/", (req, res) => {
     res.send("Home Page");
